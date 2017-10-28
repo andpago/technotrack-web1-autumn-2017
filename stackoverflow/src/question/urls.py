4f5bd1004dca3aa0fe3543ddc5fa5e5315caf9c5
andpago <andpago@gmail.com>
2017-10-28 16:29:29 +0300

    question editing

--- stackoverflow/src/question/urls.py
+++ stackoverflow/src/question/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .views import QuestionDetailView, QuestionListView, QuestionCreateView
+from .views import QuestionDetailView, QuestionListView, QuestionCreateView, QuestionEditView
 from answer.views import CreateAnswerView
 
 urlpatterns = [
     url(r'^(?P<pk>\d+)/create_answer$', CreateAnswerView.as_view(), name='create_answer'),
+    url(r'^(?P<pk>\d+)/edit$', QuestionEditView.as_view(), name='edit_question'),
     url(r'^(?P<pk>\d+)$', QuestionDetailView.as_view(), name='question'),
     url(r'^every$', QuestionListView.as_view(), name='question_list'),
     url(r'^create$', QuestionCreateView.as_view(), name='create'),
--- stackoverflow/src/question/views.py
+++ stackoverflow/src/question/views.py
@@ -1,9 +1,10 @@
 from django import forms
+from django.http import HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, ListView, CreateView, FormView
+from django.views.generic import DetailView, ListView, CreateView, FormView, UpdateView
 
 from answer.views import AnswerCreateForm
 from .models import Question
@@ -40,8 +41,29 @@ class QuestionCreateView(CreateView):
     def form_valid(self, form):
         question = form.save(commit=False)
         question.author = self.request.user
-
         question.save()
 
-        
         return super(QuestionCreateView, self).form_valid(form)
+
+
+def author_only(f):
+    def res(self, *args, **kwargs):
+        if self.get_object().author != self.request.user:
+            return HttpResponseForbidden("<h1>You cannot edit this entry</h1>")
+        else:
+            return f(self, *args, **kwargs)
+    return res
+
+
+class QuestionEditView(UpdateView):
+    template_name = 'question/question_edit.html'
+    model = Question
+    fields = ['title', 'text']
+
+    @author_only
+    def get(self, *args, **kwargs):
+        return super(QuestionEditView, self).get(*args, **kwargs)
+
+    @author_only
+    def post(self, *args, **kwargs):
+        return super(QuestionEditView, self).post(*args, **kwargs)
