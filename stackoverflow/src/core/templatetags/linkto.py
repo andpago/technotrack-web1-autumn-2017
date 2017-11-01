98191c6774b6efd4ef969e4225fea4352c34d2c0
andpago <andpago@gmail.com>
2017-11-01 20:53:36 +0300

    some more css

--- stackoverflow/src/core/templatetags/linkto.py
+++ stackoverflow/src/core/templatetags/linkto.py
@@ -1,36 +1,39 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.template import Library, Template
+from django.template.loader import render_to_string
+from django.urls import reverse
+
+from question.models import Question
+
+from question.models import Answer
 
 register = Library()
 User = get_user_model()
 
 
 @register.simple_tag(takes_context=True)
-def linkto(context, obj):
-    context.update({'object': obj})
-
-    cls = obj.__class__.__name__
-
-    if cls not in linkto.links.keys():
+def linkto(context, obj, **kwargs):
+    cls = type(obj)
+    if cls not in linkto.info_getters.keys():
         raise Exception("cannot create link to an object of type " + cls)
 
-    return Template('<a href={% url \'' + linkto.links[cls] + '\' object.' + linkto.args[cls] + ' %}>' + linkto.text[cls] + '</a>').render(context)
+    info = linkto.info_getters[type(obj)](obj)
+    info.update(kwargs)
 
-linkto.links = {
-    'User': 'core:user',
-    'Question': 'question:question',
-    'Answer': 'question:question'
-}
+    return render_to_string('core/tagtemplates/linkto.html', info)
 
-linkto.args = {
-    'User': 'username',
-    'Question': 'pk',
-    'Answer': 'question.pk'
-}
-
-linkto.text = {
-    'User': '@{{ object.username }}',
-    'Question': '{{object.title}}',
-    'Answer': '{{object.text|truncatechars:20}}'
+linkto.info_getters = {
+    User: lambda user: {
+        'text': '@' + user.username,
+        'url': reverse('core:user', kwargs={'slug': user.username})
+    },
+    Question: lambda question: {
+        'text': question.title,
+        'url': reverse('question:question', kwargs={'pk': question.id})
+    },
+    Answer: lambda answer: {
+        'text': answer.text[:15] + '...',
+        'url': reverse('question:question', kwargs={'pk': answer.question.id})
+    },
 }
